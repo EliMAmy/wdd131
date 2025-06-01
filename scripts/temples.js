db4f624210c344fa846a0ea44577ba5ded32c41f
EliMAmy <rociomarapaza@gmail.com>
2025-06-01 17:52:22 -0500

    .

--- scripts/temples.js
+++ scripts/temples.js
@@ -14,3 +14,13 @@ let oLastModif = new Date(document.lastModified);
 
 const lastModified = document.querySelector("#lastModified");
 lastModified.innerHTML = `<span class="highlight">${oLastModif}</span>`;
+
+// Store the selected elements that we are going to use. This is not required but a good practice with larger programs where the variable will be referenced more than once.
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
